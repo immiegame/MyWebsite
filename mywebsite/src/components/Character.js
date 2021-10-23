320cc385f6fa842e0debb6c7f94e63fb5776081e
immiegame <84215935+immiegame@users.noreply.github.com>
2021-10-23 01:13:31 -0400

    Component Test

---
+++ mywebsite/src/components/Character.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CharSpr from './blackrainfrog.jpeg'
+
+export default function Character() {
+    return(
+        <div>
+            <h1>Character!</h1>
+            <p>idk dis some text</p>
+            <img src={CharSpr} />
+        </div>
+    )
+}
